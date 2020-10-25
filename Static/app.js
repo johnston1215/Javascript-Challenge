48d4a26125a3279ff3f2551929ae59cf345d4f0d
Johnston1215 <johnston1215@yahoo.com>
2020-10-25 15:33:10 -0500

    Updated for full table

--- Static/app.js
+++ Static/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
+// check data
+console.log(data);
 
+// Initial loop / Append to tables
+data.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
