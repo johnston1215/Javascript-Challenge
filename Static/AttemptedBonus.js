86a0173959557153ad8a68eccdf998643d487339
Johnston1215 <johnston1215@yahoo.com>
2020-10-25 18:38:24 -0500

    FINAL

--- Static/AttemptedBonus.js
+++ Static/AttemptedBonus.js
@@ -17,13 +17,16 @@ data.forEach(function(sighting) {
   });
   
 // Select the button
-var button = d3.select("#filter-btn");
+// var button = d3.select("#filter-btn");   THIS WAS MY FINAL CODE
 // Select the form
-var form = d3.select("#form");
+// var form = d3.select("#form");    THIS WAS MY FINAL CODE
+var inputElement = d3.select("input");  // THIS WAS ADDED
 
 // Create event handlers 
-button.on("click", runEnter);
-form.on("submit", runEnter);
+// button.on("click", runEnter);  THIS WAS MY FINAL CODE
+// form.on("submit", runEnter);  THIS WAS MY FINAL CODE
+inputElement.on("change", runEnter);  // THIS WAS ADDED
+var filters = {} // THIS WAS ADDED
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -31,12 +34,17 @@ function runEnter() {
   d3.event.preventDefault();
   tbody.html("");
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  // THIS WAS MY FINAL CODE var inputElement = d3.select("#datetime");
+  // console.log("Line 35" , this.id);  // THIS WAS ADDED
+  // var inputID = this.id;  //  THIS WAS ADDED
+  filters[this.id] = this.value; //THIS WAS ADDED
+  console.log(filters) // THIS WAS ADDED
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-  console.log(inputValue);
-  var filteredData = data.filter(data => data.datetime === inputValue);
+  // THIS WAS MY FINAL CODE var inputValue = inputElement.property("value");
+  // THIS WAS MY FINAL CODE console.log(inputValue);
+  // THIS WAS MY FINAL CODE var filteredData = data.filter(data => data.datetime === inputValue);
+  var filteredData = data.filter(data => data[Object.keys(filters)[0]] === Object.values(filters)[0]); // THIS WAS ADDED
   console.log(filteredData);
   
   // Add to website
