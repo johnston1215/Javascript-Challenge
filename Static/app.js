5aa6f7ffe001fa7282c02e16eef923475abdc19f
Johnston1215 <johnston1215@yahoo.com>
2020-10-25 16:50:04 -0500

    10_25 pre-tutor

--- Static/app.js
+++ Static/app.js
@@ -3,16 +3,46 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // check data
-console.log(data);
+// console.log(data);
 
 // Initial loop / Append to tables
 data.forEach(function(sighting) {
-    console.log(sighting);
+    // console.log(sighting);
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(function([key, value]) {
-      console.log(key, value);
+      // console.log(key, value);
       var cell = row.append("td");
       cell.text(value);
     });
   });
   
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  var filteredData = data.filter(data => data.datetime === inputValue);
+  console.log(filteredData);
+
+  // var list = d3.select("tbody");
+
+  // // remove any children from the list
+  // list.html("");
+
+  // // append to the list
+  // list.append("li").text(filteredData);
+};
